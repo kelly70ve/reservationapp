a165d604775f4c7e8a957fabc35d053b886261c2
Rocky <rrsalerno21@gmail.com>
2020-06-11 21:15:52 -0700

    added alert conditionals

--- Data/waitListData.js
+++ Data/waitListData.js
@@ -1,16 +1,3 @@
-var waiting = [ 
-    {
-    "customerName": "Waitlist Sabree",
-    "phoneNumber": "234 622 4323",
-    "customerEmail": "wait hs@yahoo.com",
-    "customerID": "CodeBoy"
-    },
-    {
-    "customerName": "WAITING Enright",
-    "phoneNumber": "11-5555",
-    "customerEmail": "waiting nrightcw@gmail.com",
-    "customerID": "still waiting right"
-    } 
-]; 
+var waiting = []; 
 
 module.exports = waiting; 
